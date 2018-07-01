7923c168d17c1168a9dbe2c2cb12ca971a01729d
Lakshmipriyapriya <39649881+Lakshmipriyapriya@users.noreply.github.com>
2018-07-01 16:35:13 +0630

    Add files via upload

--- Mongo-Operations/src/resources/post_operations.js
+++ Mongo-Operations/src/resources/post_operations.js
@@ -1,8 +1,6 @@
 var User = require('../../models/user');
 var logger = require('./../../loggers/index');
-//var config=require('config');
 var request = require('request');
-
 
 var getMyPostDetails = function(req, res){
 	var postId = req.query.postId;
@@ -10,7 +8,74 @@ var getMyPostDetails = function(req, res){
 	return request(url, function(error, responsePostDetails){
 		res.setHeader("content-type", "application/json");
 		return res.send(responsePostDetails.body);
+		console.log('got details successfully');
+	})
+}
+var createMyPostDetails=function(req,res){
+	//var postId=req.query.postId;
+	var createPostDetails ={
+			userId : req.body.userId,
+			//Id : req.body.Id,
+			title : req.body.title,
+			body : req.body.body
+		}
+			
+
+	 var url = "https://jsonplaceholder.typicode.com/posts";
+	 return request.post(url, createPostDetails, function(error, responseGetDetails){
+	 	return res.send(responseGetDetails.body);
+	 })
+	} 
+
+var updateMyPostDetails= function(req,res){
+  	
+  	var updatePostDetails={
+  		userId : req.body.userId,
+		//Id : req.body.Id,
+		title : req.body.title,
+		body : req.body.body
+	}
+	var id = req.query.id;
+	var url="https://jsonplaceholder.typicode.com/posts" +id;
+	return request.put(url,updatePostDetails , function(error, responseGetDetails){
+		if(!error){
+			if (req.body.userId != null)
+			responseGetDetails.userId = req.body.userId;
+		console.log(responseGetDetails.userId);
+	// 	if (req.body.Id != null)
+	// 	responseGetDetails.Id = req.body.Id;
+	
+	// console.log(responseGetDetails.Id);
+
+		if (req.body.title != null)
+			responseGetDetails.title = req.body.title;
+		console.log(responseGetDetails.title);
+		if (req.body.body != null)
+			responseGetDetails.body = req.body.body;
+		console.log(responseGetDetails.body);
+		res.setHeader("content-type", "application/json");
+	 	return res.send({
+	 			statusCode:200,
+	 			message:"updated",
+	 			body:responseGetDetails.body
+	 		});
+	 	}
+	 })
+
+}
+var deleteMyPostDetails = function(req, res){
+	var id = req.query.id;
+	var url = "https://jsonplaceholder.typicode.com/posts/"+ id ;
+	return request(url, function(error, responseRemovedDetails){
+		console.log('details that has removed:',responseRemovedDetails.body);
+		return res.send('details that has been removed in a given id:'+" "+id);
+		console.log('deleted details successfully');
 	})
 }
 
+
+			
+exports.deleteMyPostDetails=deleteMyPostDetails
 exports.getMyPostDetails = getMyPostDetails
+exports.createMyPostDetails=createMyPostDetails
+exports.updateMyPostDetails=updateMyPostDetails
--- Mongo-Operations/test/user_post_operations.test.js
+++ Mongo-Operations/test/user_post_operations.test.js
@@ -5,6 +5,7 @@
  var userPosts = require('./../src/resources/post_operations');
  var _ = require('underscore')
  var expect= require("chai").expect;
+ var idForOperation;
 
  describe('Post Operations', function(){
  	it("should get user's post details", function(done){
@@ -21,4 +22,74 @@
  			done();
  		})
  	})
- })
+ 
+ describe('put operations', function(){
+ 	it("should create user put details",function(done){
+ 		var req=sinon.spy();
+ 		var res= sinon.spy();
+ 		res = { json: sinon.spy(), setHeader: sinon.spy(), send: sinon.spy() }
+ 		var req={
+ 			"body":{
+ 				"userId":"120",
+ 				"body":"hai their",
+ 				"title":"creating the details"
+
+ 			}
+
+ 		}
+ 		userPosts.createMyPostDetails(req,res).then(function(putDetails){
+ 			chai.expect(putDetails.userId,120);
+ 			chai.expect(putDetails.body,"hai their");
+ 			chai.expect(putDetails.title,"creating the details");
+ 			done();
+ 		})
+ 	})
+ })	
+it('should Update user details with provided data', function (done) {
+        let req = sinon.spy();
+        let res = sinon.spy();
+            //res = { json : sinon.spy() }
+            res = { send : sinon.spy(), json: sinon.spy() }
+            req = {
+              "params":{
+                "id": idForOperation
+              },
+              "body":{
+              	"userId":"10",
+ 				"body":"hai their",
+ 				"title":"updating the details"
+                
+              }
+              
+            }
+            userPosts. updateMyPostDetails(req, res).then(function(updatePostsData){
+            	chai.expect(putDetails.userId,10);
+ 			chai.expect(putDetails.body,"hai their");
+ 			chai.expect(putDetails.title,"updating the details");
+           console.log('updatePostsData....',updatePostsData);
+         done();
+        })
+      });
+it('should delete posts details with provided data', function (done) {
+         let req = sinon.spy();
+         let res = sinon.spy();
+             res = { json : sinon.spy(),send:sinon.spy() };
+             req = {
+               "params":{
+                 "id": idForOperation
+               }
+             }
+         userPosts.deleteMyPostDetails(req,res).then(function(removePostsData){
+           console.log('removePostsData', removePostsData);
+           done();
+        })
+    });
+});
+
+    
+
+
+
+
+
+
